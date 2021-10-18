c619f1326c6f0700068891213fbbbb0463ee084c
Wadim Skworzow <sempris200@gmail.com>
2021-10-18 15:38:16 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,377 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.querySelector('#answer-buttons')
+const winningPool = document.getElementById('winning')
+const checkBoxOutput = document.getElementById('checkbox')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        button.setAttribute('data-correct', answer.correct)
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.insertAdjacentElement('beforeend', button)
+    })
+    winningPool.innerText = `🏆 ${counter} 🪙`
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    checkBoxOutput.innerText = ''
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+let counter = 0;
+function counting() {
+    return function () {
+        counter += 100;
+        return counter;
+    }
+}
+let callCounter = counting();
+
+var img = document.createElement("img");
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const selectedData = selectedButton.dataset.correct
+    if (selectedData === 'true') {
+        winningPool.innerText = `🏆 ${callCounter()} 🪙`
+        img.src = "yes.gif";
+        checkBoxOutput.appendChild(img);
+    }
+    else {
+        img.src = "no.gif";
+        checkBoxOutput.appendChild(img);
+    }
+    [...answerButtonsElement.children].forEach(button => {
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct')
+        }
+        else {
+            button.classList.add('wrong')
+        }
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.add('re')
+        counter = 0;
+        startButton.classList.remove('hide')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'A person with well-developed abdominal muscles is said to have a what?',
+        answers: [
+            { text: 'A: One-pack', correct: false },
+            { text: 'B: Six-Pack', correct: true },
+            { text: 'C: 12-pack', correct: false },
+            { text: 'D: Family-pack', correct: false }
+        ]
+    },
+    {
+        question: 'Which two words traditionally appear onscreen at the termination of a feature film?',
+        answers: [
+            { text: 'A: The End', correct: true },
+            { text: 'B: The Conclusion', correct: false },
+            { text: 'C: The Finish', correct: false },
+            { text: 'D: The Pizza Rolls Are Done', correct: false }
+        ]
+    },
+    {
+        question: 'A magnet would most likely attract which of the following?',
+        answers: [
+            { text: 'A: Metal', correct: true },
+            { text: 'B: Plastic', correct: false },
+            { text: 'C: Wood', correct: false },
+            { text: 'D: The wrong man', correct: false }
+        ]
+    },
+    {
+        question: 'Which of these names is not in the title of a Shakespeare play?',
+        answers: [
+            { text: 'A: Hamlet', correct: false },
+            { text: 'B: Romeo', correct: false },
+            { text: 'C: Macbeth', correct: false },
+            { text: 'D: Darren', correct: true }
+        ]
+    },
+    {
+        question: 'Where did Scotch whisky originate?',
+        answers: [
+            { text: 'A: Ireland', correct: false },
+            { text: 'B: Wales', correct: false },
+            { text: 'C: The United States', correct: false },
+            { text: 'D: Scotland', correct: true }
+        ]
+    },
+    {
+        question: 'What name is traditionally given to the party held for a woman who is expecting a baby?',
+        answers: [
+            { text: 'A: Baby drizzle', correct: false },
+            { text: 'B: Baby shower', correct: true },
+            { text: 'C: Baby downpour', correct: false },
+            { text: 'D: Baby deluge', correct: false }
+        ]
+    },
+    {
+        question: 'In fancy hotels, it is traditional for what tantalizing treat to be left on your pillow?',
+        answers: [
+            { text: 'A: A pretzel', correct: false },
+            { text: 'B: An apple', correct: false },
+            { text: 'C: A mint', correct: true },
+            { text: 'D: A photo of Wolf Blitzer', correct: false }
+        ]
+    },
+    {
+        question: 'In the United States, what is traditionally the proper way to address a judge?',
+        answers: [
+            { text: 'A: Your holiness', correct: false },
+            { text: 'B: Your honor', correct: true },
+            { text: 'C: Your eminence', correct: false },
+            { text: 'D: You da man!', correct: false }
+        ]
+    },
+    {
+        question: 'Which of these pairs of apps offers roughly the same type of service?',
+        answers: [
+            { text: 'A: Snapchat and Grubhub', correct: false },
+            { text: 'B: Whatsapp and SHAREit', correct: false },
+            { text: 'C: TikTok and Spotify', correct: false },
+            { text: 'D: Lyft and Uber', correct: true }
+        ]
+    },
+    {
+        question: 'The popular children\'s song "It\'s Raining, It\'s Pouring" mentions an "old man" doing what?',
+        answers: [
+            { text: 'A: Snoring', correct: true },
+            { text: 'B: Cooking', correct: false },
+            { text: 'C: Laughing', correct: false },
+            { text: 'D: Yelling at squirrels', correct: false }
+        ]
+    },
+    {
+        question: 'At a restaurant, someone who "foots the bill" does what?',
+        answers: [
+            { text: 'A: Kisses it', correct: false },
+            { text: 'B: Rips it up', correct: false },
+            { text: 'C: Hopes to get lucky', correct: false },
+            { text: 'D: Pays it', correct: true }
+        ]
+    },
+    {
+        question: 'In which of these films does Whoopi Goldberg dress up as a nun?',
+        answers: [
+            { text: 'A: Sister Act', correct: true },
+            { text: 'B: Ghost', correct: false },
+            { text: 'C: The Color Purple', correct: false },
+            { text: 'D: How Judas Got His Groove Back', correct: false }
+        ]
+    },
+    {
+        question: 'If someone asked to see your ID, what might you show them?',
+        answers: [
+            { text: 'A: Your tongue', correct: false },
+            { text: 'B: Your teeth', correct: false },
+            { text: 'C: Your passport', correct: true },
+            { text: 'D: The door', correct: false }
+        ]
+    },
+    {
+        question: 'According to a common phrase, a person who takes chances is "going out on a" what?',
+        answers: [
+            { text: 'A: Limb', correct: true },
+            { text: 'B: Horse', correct: false },
+            { text: 'C: Skateboard', correct: false },
+            { text: 'D: Nude beach', correct: false }
+        ]
+    },
+    {
+        question: 'Due to the geographical areas they represented, the opposing sides of the US Civil War were known by what names?',
+        answers: [
+            { text: 'A: The Hills and the Valleys', correct: false },
+            { text: 'B: The Cities and the Farms', correct: false },
+            { text: 'C: The North and the South', correct: true },
+            { text: 'D: The Kool and the Gang', correct: false }
+        ]
+    },
+    {
+        question: 'According to the old saying, "love of" what "is the root of all evil"?',
+        answers: [
+            { text: 'A: Food', correct: false },
+            { text: 'B: Money', correct: true },
+            { text: 'C: Clothing', correct: false },
+            { text: 'D: Reality television', correct: false }
+        ]
+    },
+    {
+        question: 'A geologist would likely be LEAST helpful for answering questions about which of the following?',
+        answers: [
+            { text: 'A: Granite boulders', correct: false },
+            { text: 'B: Precious stones', correct: false },
+            { text: 'C: Igneous rocks', correct: false },
+            { text: 'D: Fruity Pebbles', correct: true }
+        ]
+    },
+    {
+        question: 'A feisty train is the hero of a classic children\'s book titled The Little Engine That what?',
+        answers: [
+            { text: 'A: Ran', correct: false },
+            { text: 'B: Cried', correct: false },
+            { text: 'C: Could', correct: true },
+            { text: 'D: Opened a can of whupass', correct: false }
+        ]
+    },
+    {
+        question: 'A person who is not a banker and lends money at an extremely high interest rate is known as what?',
+        answers: [
+            { text: 'A: Loan shark', correct: true },
+            { text: 'B: Green snake', correct: false },
+            { text: 'C: Paper tiger', correct: false },
+            { text: 'D: Brother-in-law', correct: false }
+        ]
+    },
+    {
+        question: 'A well-known lyric in the holiday song "Silver Bells" promises that "soon it will be" what?',
+        answers: [
+            { text: 'A: July 4th weekend', correct: false },
+            { text: 'B: Halloween night', correct: false },
+            { text: 'C: Christmas Day', correct: true },
+            { text: 'D: National Burrito Month', correct: false }
+        ]
+    },
+    {
+        question: 'When a person is rudely ignored, he is said to be getting what?',
+        answers: [
+            { text: 'A: Hot knee', correct: false },
+            { text: 'B: Warm toe', correct: false },
+            { text: 'C: Cold shoulder', correct: true },
+            { text: 'D: His car fixed', correct: false }
+        ]
+    },
+    {
+        question: 'A common piece of advice goes, "Be there or be" what?',
+        answers: [
+            { text: 'A: Bare', correct: false },
+            { text: 'B: Square', correct: true },
+            { text: 'C: Aware', correct: false },
+            { text: 'D: All alone as usual', correct: false }
+        ]
+    },
+    {
+        question: '"And many more" is a line commonly sung at the end of what traditional ditty?',
+        answers: [
+            { text: 'A: Old McDonald', correct: false },
+            { text: 'B: Jolly Good Fellow', correct: false },
+            { text: 'C: Happy Birthday', correct: true },
+            { text: 'D: Home on the Range', correct: false }
+        ]
+    },
+    {
+        question: 'If you\'re trying to find other players in a game of hide-and-seek, what are you most likely called?',
+        answers: [
+            { text: 'A: Butterbean', correct: false },
+            { text: 'B: Stinky', correct: false },
+            { text: 'C: Dunce', correct: false },
+            { text: 'D: It', correct: true }
+        ]
+    },
+    {
+        question: 'Which of these phrases is slang for "someone who has a strong and unyielding attitude"?',
+        answers: [
+            { text: 'A: Boiled ham', correct: false },
+            { text: 'B: Tough cookie', correct: true },
+            { text: 'C: Ruffled chip', correct: false },
+            { text: 'D: Soggy cereal', correct: false }
+        ]
+    },
+    {
+        question: 'If you\'re skeptical about something, you should "take it with a grain of" what?',
+        answers: [
+            { text: 'A: Sand', correct: false },
+            { text: 'B: Sugar', correct: false },
+            { text: 'C: Salt', correct: true },
+            { text: 'D: MSG', correct: false }
+        ]
+    },
+    {
+        question: 'Something in an obvious location is said to be "right under your" what?',
+        answers: [
+            { text: 'A: Mattress', correct: false },
+            { text: 'B: Nose', correct: true },
+            { text: 'C: Azaleas', correct: false },
+            { text: 'D: Boxer shorts', correct: false }
+        ]
+    },
+    {
+        question: 'When a tree is cut down, the part that remains in the ground is called what?',
+        answers: [
+            { text: 'A: Rump', correct: false },
+            { text: 'B: Hump', correct: false },
+            { text: 'C: Stump', correct: true },
+            { text: 'D: Leftovers', correct: false }
+        ]
+    },
+    {
+        question: 'To convince you he is truthful, a person is most likely to say he will "swear on a stack of" what?',
+        answers: [
+            { text: 'A: Gold', correct: false },
+            { text: 'B: Bibles', correct: true },
+            { text: 'C: Flags', correct: false },
+            { text: 'D: Pancakes', correct: false }
+        ]
+    },
+    {
+        question: 'Where would you most likely hear the request "Cleanup in aisle 5"?',
+        answers: [
+            { text: 'A: Savings bank', correct: false },
+            { text: 'B: Supermarket', correct: true },
+            { text: 'C: Kindergarten', correct: false },
+            { text: 'D: Roller coaster', correct: false }
+        ]
+    },
+    {
+        question: 'By definition, a 10-speed bike has 10 what?',
+        answers: [
+            { text: 'A: Wheels', correct: false },
+            { text: 'B: Spokes', correct: false },
+            { text: 'C: Gears', correct: true },
+            { text: 'D: Lives', correct: false }
+        ]
+    },
+]
